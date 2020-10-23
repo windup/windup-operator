ea2edf8d489b80c1da643939f4a5b1a358fb0395
jonathan <jvilalop@redhat.com>
2020-10-23 12:46:26 +0200

    Deleted the CR creation from script create all, renamed the CR

--- src/main/resources/k8s/def/script.create.all.sh
+++ src/main/resources/k8s/def/script.create.all.sh
@@ -10,5 +10,5 @@ echo "Creating operator deployment"
 kubectl apply -f ./windup.deployment.yaml
 echo "Creating CRD"
 kubectl apply -f ./windup.crd.yaml
-echo "Creating CR ... should start windups deployment"
-kubectl apply -f ../examples/windup-v4.0.2.yaml
+#echo "Creating CR ... should start windups deployment"
+#kubectl apply -f ../examples/windup-v4.0.2.yaml
