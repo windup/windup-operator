e3ccb4cda828fb10159b16a393f1236d4c6fac35
jonathan <jvilalop@redhat.com>
2020-10-26 19:08:57 +0100

    Cleaned imports

--- src/main/java/org/jboss/windup/operator/WindupOperator.java
+++ src/main/java/org/jboss/windup/operator/WindupOperator.java
@@ -4,7 +4,6 @@ import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 import io.quarkus.runtime.StartupEvent;
 import lombok.extern.java.Log;
-import org.jboss.logging.Logger;
 import org.jboss.windup.operator.model.WindupResource;
 import org.jboss.windup.operator.model.WindupResourceDoneable;
 import org.jboss.windup.operator.model.WindupResourceList;
