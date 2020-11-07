35517075fe8e6fe0c2ab8f8ed4133bf07f3586d5
jonathan <jvilalop@redhat.com>
2020-11-07 11:40:44 +0100

    Added test file

---
+++ src/test/resources/local-test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -x
+# you need to have minikube/kind running
+# be sure to verify you are in the right context with `kubectl config current-context`
+# you can change it with `kubectl config use-context`
+
+cd ../../..
+containertag=$RANDOM
+echo "containertag : $containertag"
+mvn clean package -Pnative -Dquarkus.container-image.build=true -DskipTests "-Dquarkus.container-image.tag=$containertag"
+# for minikube replace next line with : eval $(minikube -p minikube docker-env)
+kind load docker-image quay.io/windupeng/windup-operator-native:$containertag
+
+cd src/main/resources/k8s/def 
+./script.delete.all.sh
+kubectl apply -f windup.namespace.yaml 
+kubectl apply -f windup.serviceaccount.yaml 
+kubectl apply -f windup.role.yaml 
+kubectl apply -f windup.rolebinding.yaml 
+kubectl apply -f windup.crd.yaml 
+sed "s/windup-operator-native:latest/windup-operator-native:$containertag/g" ../../../../test/resources/windup.deployment.yaml | kubectl apply -f -
+kubectl apply -f ../../../../test/resources/windup-test.yaml
+sleep 20
+kubectl get all,ing,pvc -n mta -o name
+num=`kubectl get all,ing,pvc -n mta -o name | wc -l`
+echo "num $num"
+if [ "$num" -gt "18" ];
+  then echo "Test not passed";
+fi
+if [ "$num" -eq "18" ];
+  then echo "Test OK";
+fi
