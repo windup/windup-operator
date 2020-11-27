b98db818f530b19eb4d792536337d9b1f22174c2
jonathan <jvilalop@redhat.com>
2020-11-27 15:46:59 +0100

    [PR] wording and format reviewed on the values for the CSV

--- src/main/resources/operatorhub/delete-operator.sh
+++ src/main/resources/operatorhub/delete-operator.sh
@@ -3,5 +3,5 @@
  kubectl delete catalogsource -n openshift-marketplace --field-selector metadata.name=test-catalog
  kubectl delete crd windups.windup.jboss.org
  kubectl delete windup --all -A
- kubectl delete deployment -A --field-selector metadata.name=windup-operator.v0.0.1
- kubectl delete csv -A --field-selector metadata.name=windup-operator.v0.0.1
+ kubectl delete deployment -A --field-selector metadata.name=windup-operator.0.0.1
+ kubectl delete csv -A --field-selector metadata.name=windup-operator.0.0.1
