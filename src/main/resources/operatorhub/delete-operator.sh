a3ec796f6306b8fc72f8299248b002b0a07eec6d
jonathan <jvilalop@redhat.com>
2021-01-18 15:04:26 +0100

    Created v0.0.2 of the operator

--- src/main/resources/operatorhub/delete-operator.sh
+++ src/main/resources/operatorhub/delete-operator.sh
@@ -1,7 +1,7 @@
  # script to delete everything in the cluster related to the Operator
  kubectl delete subscription -A --field-selector metadata.name=windup
- kubectl delete catalogsource -n openshift-marketplace --field-selector metadata.name=test-catalog
+ kubectl delete catalogsource -n openshift-marketplace -l application=windup
  kubectl delete crd windups.windup.jboss.org
  kubectl delete windup --all -A
- kubectl delete deployment -A --field-selector metadata.name=windup-operator.0.0.1
- kubectl delete csv -A --field-selector metadata.name=windup-operator.0.0.1
+ kubectl delete deployment -A --field-selector metadata.name=windup-operator.0.0.2
+ kubectl delete csv -A --field-selector metadata.name=windup-operator.0.0.2
