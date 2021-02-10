81f67f8b05c139c365b9a928883558a9cc92483c
jonathan <jvilalop@redhat.com>
2021-02-10 09:53:49 +0100

    refactored create-catalog script

--- src/main/resources/operatorhub/create-catalog.sh
+++ src/main/resources/operatorhub/create-catalog.sh
@@ -37,16 +37,9 @@ fi
 # make build
 
 # Build operator catalog
-# if version on argument is 0.0.1 we will not create the catalog from the community one as it already has that version and would crash
-if [ "0.0.1" = "$mtaoperatorversion" ]; then
-# Create operator test bundle image with the windupeng 0.0.1 operator image
-podman build -f mta-operator/$mtaoperatorversion/Dockerfile -t mta-operator-bundle:$bundleversion mta-operator/$mtaoperatorversion/
-podman tag mta-operator-bundle:$bundleversion quay.io/$quayuser/mta-operator-bundle:$bundleversion
-podman push quay.io/$quayuser/mta-operator-bundle:$bundleversion
-
-../../../../../operator-registry/bin/opm index add --bundles quay.io/$quayuser/mta-operator-bundle:$bundleversion \
---tag quay.io/$quayuser/mta-operator-test-catalog:$bundleversion --container-tool podman
-else
+# if version on argument is the latest version , not published yet, it will create the catalog from the community one to include past published versions
+# it will also mean the container image for the operator will be the one on the $user quay account
+if [ "0.0.2" == "$mtaoperatorversion" ]; then
 # Create operator bundle image using user quay image for operator
 sed -i "s/quay.io\/windupeng/quay.io\/$quayuser/g" "mta-operator/$mtaoperatorversion/manifests/windup-operator.v$mtaoperatorversion.clusterserviceversion.yaml"
 podman build -f mta-operator/$mtaoperatorversion/Dockerfile -t mta-operator-bundle:$bundleversion mta-operator/$mtaoperatorversion/
@@ -57,6 +50,16 @@ sed -i "s/quay.io\/$quayuser/quay.io\/windupeng/g" "mta-operator/$mtaoperatorver
 ../../../../../operator-registry/bin/opm index add --bundles quay.io/$quayuser/mta-operator-bundle:$bundleversion \
 --tag quay.io/$quayuser/mta-operator-test-catalog:$bundleversion --container-tool podman \
 --from-index quay.io/openshift-community-operators/catalog:latest 
+else 
+# Using an already published version means we are not going to include the community-operators catalog as that one already has this version
+# Create operator test bundle image with the windupeng 0.0.1 operator image
+podman build -f mta-operator/$mtaoperatorversion/Dockerfile -t mta-operator-bundle:$bundleversion mta-operator/$mtaoperatorversion/
+podman tag mta-operator-bundle:$bundleversion quay.io/$quayuser/mta-operator-bundle:$bundleversion
+podman push quay.io/$quayuser/mta-operator-bundle:$bundleversion
+
+../../../../../operator-registry/bin/opm index add --bundles quay.io/$quayuser/mta-operator-bundle:$bundleversion \
+--tag quay.io/$quayuser/mta-operator-test-catalog:$bundleversion --container-tool podman
+
 fi
 
 podman push  quay.io/$quayuser/mta-operator-test-catalog:$bundleversion
