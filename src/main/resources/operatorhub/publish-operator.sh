40291dc12c2cdcfd706fc37d7c739a048b01f3fd
jonathan <jvilalop@redhat.com>
2021-02-10 19:00:06 +0100

    Adding signature and PR creation

--- src/main/resources/operatorhub/publish-operator.sh
+++ src/main/resources/operatorhub/publish-operator.sh
@@ -2,9 +2,12 @@ while getopts u:v:m: flag
 do
     case "${flag}" in
         u) githubuser=${OPTARG};;
-        m) mtaoperatorversion=${OPTARG};;
+        v) newversion=${OPTARG};;
     esac
 done
+
+# Install GitHub cli to be able to create the pull request
+# https://github.com/cli/cli
 
 # clone community-operators user fork
 git clone git@github.com:$githubuser/community-operators.git
@@ -14,15 +17,15 @@ git remote add upstream git@github.com:operator-framework/community-operators.gi
 cd community-operators
 git branch -b "mta-operator-$mtaoperatorversion" master
 
-# copy files from windup-operator, for the certain version
-cp ../mta-operator/$mtaoperatorversion  ./community-operators/mta-operator 
+# copy files from windup-operator, for the specific version
+cp ../mta-operator/$newversion  ./community-operators/mta-operator/$newversion
 
 # commit
 git add --all ./community-operators/mta-operator 
-git commit -am "Upgrade MTA Operator to $mtaoperatorversion in community-operators"
+git commit -am "Upgrade MTA Operator to $mtaoperatorversion in community-operators" -S
 
 # push
 git push --set-upstream origin "mta-operator-$mtaoperatorversion"
 
 # create pull request
-git request-pull upstream master 
+gh pr create --title "Upgrade MTA Operator to $mtaoperatorversion in community-operators" --base master --body "$(cat publish-pr-body.md)"
