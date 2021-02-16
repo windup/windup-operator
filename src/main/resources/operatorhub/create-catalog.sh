ba02d31bb9a49bc932d2bb4de70485d6f35460cf
Jonathan Vila <jvilalop@redhat.com>
2021-02-16 12:53:30 +0100

    windup 2998 automate publish (#22)
    * Created parameterised script to create PR to community-operators
    
    * Adding signature and PR creation
    
    * Moving arguments on the commit
    
    * Adding shebang to script
    
    * Added documentation to the script
    
    Signed-off-by: jonathan <jvilalop@redhat.com>
    
    * Added PR body file
    
    Signed-off-by: jonathan <jvilalop@redhat.com>
    
    * Upgrade MTA Operator to  in community-operators
    
    Signed-off-by: jonathan <jvilalop@redhat.com>
    
    * Changed parameter usage
    
    * Adding clean part to the script
    
    * Changed the gh command to open the web browser to confirm
    
    * Fixed create-catalog script
    
    * Removed unused properties
    Improved script to deploy the new catalog
    
    * Removed unused option

--- src/main/resources/operatorhub/create-catalog.sh
+++ src/main/resources/operatorhub/create-catalog.sh
@@ -1,40 +1,47 @@
-#!/bin/sh -x
-
-# everytime you test you need to increase this bundle version number
-# it only affects your test, has no other purposes
-# usage `./create-catalog.sh -u myuser -v 210 -m 0.0.2 -b 1`
-# -u : quay user
-# -v : version of the catalog
-# -m : version of the MTA Operator
-# -b : do the operator build first ? 1 yes , 0 no
-
-while getopts u:v:m:b: flag
-do
-    case "${flag}" in
-        u) quayuser=${OPTARG};;
-        v) bundleversion=${OPTARG};;
-        m) mtaoperatorversion=${OPTARG};;
-        b) dobuild=${OPTARG};;
-    esac
-done
-
-# Do the build of the operator with current code, if requested
-if [ "1" = "$dobuild" ]; then 
-  cd ../../../..
-  ./mvnw clean package -Pnative -DskipTests \
-            -Dquarkus.native.container-build=true \
-            -Dquarkus.native.container-runtime=podman \
-            -Dquarkus.container-image.build=true  \
-            -Dquarkus.container-image.tag=$mtaoperatorversion \
-            -Dquarkus.container-image.push=true \
-            -Dquarkus.container-image.group=$quayuser
-  cd src/main/resources/operatorhub
-fi
+#!/bin/sh 
 
 # Install operator-registry
 # git clone https://github.com/operator-framework/operator-registry
 # cd operator-registry
 # make build
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage `./create-catalog.sh -u myuser -p passwd  -m 0.0.2 -b 1`
+ -u : quay user 
+ -p : quay password 
+ -m : version of the MTA Operator
+ -b : do the operator build first ? 1 yes , 0 no'
+    exit 0
+fi
+
+set -x
+
+while getopts u:p:m:b: flag
+do
+    case "${flag}" in
+        u) quayuser=${OPTARG};;
+        p) quaypwd=${OPTARG};;
+        m) mtaoperatorversion=${OPTARG};;
+        b) dobuild=${OPTARG};;
+    esac
+done
+
+bundleversion=$RANDOM
+
+# podman login
+podman login quay.io -u $quayuser -p $quaypwd
+
+# Do the build of the operator with current code, if requested
+if [ "1" = "$dobuild" ]; then 
+
+cd ../../../..
+./mvnw clean package -Pnative -DskipTests \
+            -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=podman \
+            -Dquarkus.container-image.push=true \
+            -Dquarkus.container-image.tag=$mtaoperatorversion -Dquarkus.container-image.group=$quayuser \
+            -Dquarkus.container-image.username=$quayuser -Dquarkus.container-image.password=$quaypwd
+cd src/main/resources/operatorhub
+fi
 
 # Build operator catalog
 # if version on argument is the latest version , not published yet, it will create the catalog from the community one to include past published versions
@@ -44,17 +51,19 @@ if [ "0.0.2" == "$mtaoperatorversion" ]; then
 sed -i "s/quay.io\/windupeng/quay.io\/$quayuser/g" "mta-operator/$mtaoperatorversion/manifests/windup-operator.v$mtaoperatorversion.clusterserviceversion.yaml"
 podman build -f mta-operator/$mtaoperatorversion/Dockerfile -t mta-operator-bundle:$bundleversion mta-operator/$mtaoperatorversion/
 podman tag mta-operator-bundle:$bundleversion quay.io/$quayuser/mta-operator-bundle:$bundleversion
+
 podman push quay.io/$quayuser/mta-operator-bundle:$bundleversion
 sed -i "s/quay.io\/$quayuser/quay.io\/windupeng/g" "mta-operator/$mtaoperatorversion/manifests/windup-operator.v$mtaoperatorversion.clusterserviceversion.yaml"
 
 ../../../../../operator-registry/bin/opm index add --bundles quay.io/$quayuser/mta-operator-bundle:$bundleversion \
 --tag quay.io/$quayuser/mta-operator-test-catalog:$bundleversion --container-tool podman \
---from-index quay.io/openshift-community-operators/catalog:latest 
+--from-index quay.io/openshift-community-operators/catalog:latest
 else 
 # Using an already published version means we are not going to include the community-operators catalog as that one already has this version
 # Create operator test bundle image with the windupeng 0.0.1 operator image
 podman build -f mta-operator/$mtaoperatorversion/Dockerfile -t mta-operator-bundle:$bundleversion mta-operator/$mtaoperatorversion/
 podman tag mta-operator-bundle:$bundleversion quay.io/$quayuser/mta-operator-bundle:$bundleversion
+
 podman push quay.io/$quayuser/mta-operator-bundle:$bundleversion
 
 ../../../../../operator-registry/bin/opm index add --bundles quay.io/$quayuser/mta-operator-bundle:$bundleversion \
@@ -62,9 +71,8 @@ podman push quay.io/$quayuser/mta-operator-bundle:$bundleversion
 
 fi
 
-podman push  quay.io/$quayuser/mta-operator-test-catalog:$bundleversion
+podman push quay.io/$quayuser/mta-operator-test-catalog:$bundleversion
 
 sleep 20
 
-kubectl delete catalogsource test-catalog -n openshift-marketplace
-sed "s/{user}/$quayuser/g" catalog-source.yaml | sed "s/{version}/$bundleversion/g" | kubectl create -f -
+sed "s/{user}/$quayuser/g" catalog-source.yaml | sed "s/{version}/$bundleversion/g" | kubectl apply -f -
