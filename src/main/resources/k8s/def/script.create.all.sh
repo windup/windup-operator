306a9e7baa664dedf77a6bea55096c9ae5da4235
jonathan <jvilalop@redhat.com>
2021-02-23 18:54:59 +0100

    Added individual yamls to create the clusterrole on manual deployment

--- src/main/resources/k8s/def/script.create.all.sh
+++ src/main/resources/k8s/def/script.create.all.sh
@@ -4,6 +4,10 @@ echo "Creating role"
 kubectl apply -f ./windup.role.yaml
 echo "Creating rolebinding"
 kubectl apply -f ./windup.rolebinding.yaml
+echo "Creating cluster role"
+kubectl apply -f ./windup.clusterrole.yaml
+echo "Creating cluster rolebinding"
+kubectl apply -f ./windup.clusterrolebinding.yaml
 echo "Creating operator deployment"
 kubectl apply -f ./windup.deployment.yaml
 echo "Creating CRD"
