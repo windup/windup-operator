dbea5322c860c5850be4a808659c78bdc527961e
Jonathan Vila <jvilalop@redhat.com>
2021-04-14 16:07:58 +0200

    operatorhub version 0.0.5
    Signed-off-by: Jonathan Vila <jvilalop@redhat.com>

--- src/main/resources/operatorhub/delete-operator.sh
+++ src/main/resources/operatorhub/delete-operator.sh
@@ -3,5 +3,5 @@
  kubectl delete catalogsource -n openshift-marketplace -l application=windup
  kubectl delete crd windups.windup.jboss.org
  kubectl delete windup --all -A
- kubectl delete deployment -A --field-selector metadata.name=windup-operator.0.0.2
- kubectl delete csv -A --field-selector metadata.name=windup-operator.0.0.2
+ kubectl delete deployment -A --field-selector metadata.name=windup-operator.0.0.5
+ kubectl delete csv -A --field-selector metadata.name=windup-operator.0.0.5
