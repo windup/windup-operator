2fa03808ad7a0be30693e2bfdd2ffdd1bb62f2f8
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-04-03 14:55:43 +0200

    Test native image

---
+++ src/test/java/org/jboss/windup/operator/controllers/NativeWindupReconcilerIT.java
@@ -0,0 +1,7 @@
+package org.jboss.windup.operator.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class NativeWindupReconcilerIT extends WindupReconcilerTest {
+}
