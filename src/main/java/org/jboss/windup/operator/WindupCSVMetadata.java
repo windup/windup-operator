99b7739a87c4e592a40828a290041c55fc9687f8
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-04-20 11:57:36 +0200

    Reduce noisy Testing only CSV data

--- src/main/java/org/jboss/windup/operator/WindupCSVMetadata.java
+++ src/main/java/org/jboss/windup/operator/WindupCSVMetadata.java
@@ -4,26 +4,11 @@ import io.quarkiverse.operatorsdk.bundle.runtime.CSVMetadata;
 import io.quarkiverse.operatorsdk.bundle.runtime.SharedCSVMetadata;
 
 @CSVMetadata(
-        annotations = @CSVMetadata.Annotations(
-                containerImage = "quay.io/windupeng/windup-operator-native:${RELEASE_VERSION}",
-                repository = "https://github.com/windup/windup-operator",
-                categories = "Modernization & Migration",
-                capabilities = "Basic Install",
-                almExamples = """
-                        [{
-                          "kind": "Windup",
-                          "apiVersion": "windup.jboss.org/v1",
-                          "metadata": {
-                            "name": "windup"
-                          },
-                          "spec": {}
-                        }]
-                        """
-        ),
+        displayName = "Windup Operator",
         permissionRules = {
                 @CSVMetadata.PermissionRule(
                         apiGroups = {""},
-                        resources = {"pods", "services", "services/finalizers", "endpoints", "persistentvolumeclaims", "events", "configmaps", "secrets"},
+                        resources = {"pods", "persistentvolumeclaims", "services", "configmaps", "secrets"},
                         verbs = {"*"}
                 ),
                 @CSVMetadata.PermissionRule(
@@ -33,39 +18,22 @@ import io.quarkiverse.operatorsdk.bundle.runtime.SharedCSVMetadata;
                 ),
                 @CSVMetadata.PermissionRule(
                         apiGroups = {"networking.k8s.io"},
-                        resources = {"ingresses", "networkpolicies"},
+                        resources = {"ingresses"},
                         verbs = {"*"}
                 ),
                 @CSVMetadata.PermissionRule(
-                        apiGroups = {"apps"},
+                        apiGroups = {"apps", "extensions"},
                         resources = {"deployments"},
                         verbs = {"*"}
                 )
         },
-        displayName = "Windup Operator",
-        description = """
-                Windup is a web console application that supports large-scale Java application modernization and migration projects across a broad range of transformations and use cases.
-                                                                                                              
-                It analyzes application code, supports effort estimation, accelerates code migration, and enables users to move applications to containers.
-                                                                                                              
-                For more information please refer to the https://windup.github.io/ page.
-                               
-                 """,
         installModes = {
                 @CSVMetadata.InstallMode(type = "OwnNamespace", supported = true),
                 @CSVMetadata.InstallMode(type = "SingleNamespace", supported = false),
                 @CSVMetadata.InstallMode(type = "MultiNamespace", supported = false),
                 @CSVMetadata.InstallMode(type = "AllNamespaces", supported = false)
         },
-        keywords = {"windup", "migration", "modernization"},
-        maturity = "alpha",
-        provider = @CSVMetadata.Provider(name = "Windup"),
-        links = {
-                @CSVMetadata.Link(name = "Website", url = "windup.github.io/"),
-                @CSVMetadata.Link(name = "Github", url = "https://github.com/windup/windup-operator")
-        },
-        icon = @CSVMetadata.Icon(fileName = "icon.png", mediatype = "image/png"),
-        maintainers = {@CSVMetadata.Maintainer(name = "Windup", email = "migrate@redhat.com")}
+        icon = @CSVMetadata.Icon(fileName = "icon.png", mediatype = "image/png")
 )
 public class WindupCSVMetadata implements SharedCSVMetadata {
 }
