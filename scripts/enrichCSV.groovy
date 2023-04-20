688ac327435a209d638cb1b100b2bc27fb956c25
carlosthe19916 <2582866+carlosthe19916@users.noreply.github.com>
2023-04-20 11:56:29 +0200

    Enhance windup-operator.clusterserviceversion.yaml enrichment

--- scripts/enrichCSV.groovy
+++ scripts/enrichCSV.groovy
@@ -1,26 +1,18 @@
 @Grab('org.yaml:snakeyaml:1.33')
 import org.yaml.snakeyaml.*
 import groovy.yaml.*
-import java.time.LocalDate
+import java.util.Map
 
 def file = new File(this.args[0])
-def version = this.args[1]
 
 def fileReader = new FileReader(file)
 def yaml = new Yaml().load(fileReader)
 
-yaml.metadata.name = 'windup-operator.v' + version
-yaml.spec.annotations.containerImage = 'quay.io/windupeng/windup-operator-native:' + version
-yaml.spec.annotations.description = 'Windup Operator.'
-yaml.spec.annotations.createdAt = LocalDate.now().toString()
-// yaml.spec.install.spec.deployments[0].spec.template.spec.containers[0].image = 'quay.io/windupeng/windup-operator-native:' + version
-yaml.spec.version = version
-yaml.spec.customresourcedefinitions.owned[0].displayName = 'Windup'
-yaml.spec.customresourcedefinitions.owned[0].description = 'Windup'
-
-// Workaround for moving annotations since OperatorHub.io complains about it
-yaml.metadata.annotations = yaml.spec.annotations
-yaml.spec.annotations = ""
+// Adding cluster permissions to be able to fetch host domain
+yaml.spec.install.spec.clusterPermissions.rules[0][1] = [:]
+yaml.spec.install.spec.clusterPermissions.rules[0][1].apiGroups = ['config.openshift.io']
+yaml.spec.install.spec.clusterPermissions.rules[0][1].resources = ['ingresses']
+yaml.spec.install.spec.clusterPermissions.rules[0][1].verbs = ['get', 'list']
 
 DumperOptions options = new DumperOptions();
 options.indent = 2
